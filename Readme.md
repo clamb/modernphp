3215760a1aa629fe65fea9cac5432d084c478249
Christophe Lambercy <cly@cpnv.ch>
2017-04-23 20:27:32 +0200

    Define www's owner/group as www-data so that wordpress can upload files

---
+++ Readme.md
@@ -0,0 +1,38 @@
+Information
+===========
+
+Modernphp est une machine virtuelle pour le développement web dans le cadre
+des cours d'informatique web du CPNV.
+
+Les logiciels suivants sont installés :
+
+* Ubuntu 16.04
+* Apache2
+* MariaDB Server (dernière version au moment de l'installation)
+* PHP 7.0 (avec mysql, intl, mbstring, json, cli et xml
+
+
+Contenu
+=======
+
+* Readme.md : ce fichier
+* Vagrantfile : le fichier de configuration de la machine virtuelle
+* modernphp.local.conf : le fichier de configuration d'apache
+* setup.sh : le shell bash qui est exécuté dans la VM lorsqu'elle est provisionnée 
+* htdocs/index.html : un fichier d'exemple
+* htdocs/phpinfo.php : pour obtenir les fichiers de configuration
+
+Installation
+============
+
+1. Créer un répertoire `vagrant` sur votre disque (à la racine de `C:\\` sur Windows, dans votre home sur MacOS).
+2. Copier les fichiers `Vagrantfile`, `modernphp.local.conf` et `setup.sh` dans le dossier `vagrant` créé ci-dessus
+3. Ouvrez une fenêtre de terminal (ou ligne de commande) et se déplacer dans le répertoire `vagrant`
+   (`cd C:\\vagrant` avec Windows, `cd ~/vagrant` avec MacOS).
+4. `vagrant plugin install vagrant-auto_netword`.
+5. `vagrant plugin install vagrant-hostmanager`.
+6. Pour démarrer le téléchargement et l'installation de la machine virtuelle, exécuter la commande `vagrant up`.
+7. Le plugin `hostmanager` permet de mettre à jour votre fichier `hosts` pour que l'adresse `modernphp.local` soit
+   dirigé vers votre machine. Pour cela, exécuter la commande `vagrant hostmanager`.
+8. Vérifier l'installation en vous connectant sur votre machine : `vagrant ssh`.
+9. Vérifier le bon fonctionnement de la machine et des applications en ouvrant l'adresse `http://modernphp.local/index.html`
---
+++ setup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Autheur: Christophe Lambercy (christophe.lambercy@cpnv.ch)
+# Date: 04.04.2017
+# Description: 
+#   Ce script installe les packages nécessaires pour du développement
+#   web dans le cadre des cours web de la média. Il est appelé par 
+#   le fichier VagrantFile.
+
+VAGRANTDIR=/vagrant
+SERVERDIR=/var/www/modernphp
+
+apt-get update
+apt-get install -y vim
+apt-get install -y apache2
+apt-get install -y libapache2-mod-php7.0
+apt-get install -y mariadb-server
+apt-get install -y php7.0
+apt-get install -y php7.0-mysql
+apt-get install -y php7.0-intl
+apt-get install -y php7.0-mbstring
+apt-get install -y php7.0-json
+apt-get install -y php7.0-cli
+apt-get install -y php7.0-xml
+
+sudo cp $VAGRANTDIR/modernphp.local.conf /etc/apache2/sites-available/modernphp.local.conf
+sudo ln -sf /etc/apache2/sites-available/modernphp.local.conf /etc/apache2/sites-enabled/modernphp.local.conf
+sudo rm -rf /etc/apache2/sites-enabled/000-default.conf
+
+# Utilise le php.ini de développement
+sudo mv /etc/php/7.0/apache2/php.ini /etc/php/7.0/apache2.php.ini.bk
+sudo cp /usr/lib/php/7.0/php.ini-development /etc/php/7.0/apache2/php.ini
+
+sudo systemctl restart apache2.service
+
+if [ ! -e $SERVERDIR ]
+then
+  mkdir $SERVERDIR
+fi
+
+# Mariadb stuff
+# By default, mysql uses the plugin "unix_socket" for root. Therefore, you need to sudo when using mysql.
+/usr/bin/mysqladmin -s -u root password 'cpnv4321' || echo "** Mot de passe probablement déjà défini **"
+
+echo "Crée une base de données pour le web (cpnvdev)"
+echo "create database if not exists cpnvdev" | mysql -u root -pcpnv4321
+echo "grant all privileges on cpnvdev.* to 'cpnvdev'@'localhost' identified by 'cpnv1234' with grant option" | mysql -u root -pcpnv4321
+
+echo "Crée un base de données pour wordpress (wp)"
+echo "create database if not exists wp" | mysql -u root -pcpnv4321
+echo "grant all privileges on wp.* to 'cpnvdev'@'localhost' identified by 'cpnv1234' with grant option" | mysql -u root -pcpnv4321
+
+echo "flush privileges" | mysql -u root -pcpnv4321
+
