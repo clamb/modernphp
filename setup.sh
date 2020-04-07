53f400bf0170c800995b552a946b500ec67dcf3d
Christophe Lambercy <media-cpnv-ch@users.noreply.github.com>
2020-04-07 22:44:40 +0200

    PHP 7.3

--- setup.sh
+++ setup.sh
@@ -8,41 +8,39 @@
 #   le fichier Vagrantfile.
 
 VAGRANTDIR=/vagrant
-SERVERDIR=/var/www/modernphp
+SERVERDIR=/var/www/bantam
 SSLCERTDIR=/etc/ssl/crt
 
-# This is to install mariadb 10.4 (18.04 comes with 10.1 by default)
-apt-get install software-properties-common
-apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-add-apt-repository "deb [arch=amd64,arm64,ppc64el] http://mariadb.mirror.liquidtelecom.com/repo/10.4/ubuntu $(lsb_release -cs) main"
+# Required for older php version
+add-apt-repository -y ppa:ondrej/php
 
 apt-get update
+apt-get upgrade -y
 apt-get install -y vim
 apt-get install -y apache2
-apt-get install -y libapache2-mod-php7.2
-apt-get install -y mariadb-server mariadb-client
-apt-get install -y php7.2
-apt-get install -y php7.2-mysql
-apt-get install -y php7.2-intl
-apt-get install -y php7.2-mbstring
-apt-get install -y php7.2-json
-apt-get install -y php7.2-cli
-apt-get install -y php7.2-xml
-apt-get install -y php7.2-gd
-apt-get install -y php7.2-curl
-apt-get install -y php7.2-zip
+apt-get install -y libapache2-mod-php7.3
+apt-get install -y mariadb-server
+apt-get install -y php7.3
+apt-get install -y php7.3-mysql
+apt-get install -y php7.3-intl
+apt-get install -y php7.3-mbstring
+apt-get install -y php7.3-json
+apt-get install -y php7.3-cli
+apt-get install -y php7.3-xml
+apt-get install -y php7.3-gd
+apt-get install -y php7.3-curl
 
 # Some cleanup
 apt autoremove -y
 
-sudo cp $VAGRANTDIR/modernphp.local.conf /etc/apache2/sites-available/modernphp.local.conf
-sudo ln -sf /etc/apache2/sites-available/modernphp.local.conf /etc/apache2/sites-enabled/modernphp.local.conf
+sudo cp $VAGRANTDIR/bantam.local.conf /etc/apache2/sites-available/bantam.local.conf
+sudo ln -sf /etc/apache2/sites-available/bantam.local.conf /etc/apache2/sites-enabled/bantam.local.conf
 sudo rm -rf /etc/apache2/sites-enabled/000-default.conf
 
 echo "Creating self-signed certificate"
 sudo rm -rf $SSLCERTDIR
 sudo mkdir $SSLCERTDIR
-sudo openssl req -x509 -nodes -days 3650 -subj '/C=CH/ST=Vaud/L=Ste-Croix/CN=modernphp.local/O=CPNV/OU=Media' -newkey rsa:2048 -keyout $SSLCERTDIR/modernphp.local.key -out $SSLCERTDIR/modernphp.local.crt
+sudo openssl req -x509 -nodes -days 3650 -subj '/C=CH/ST=Vaud/L=Ste-Croix/CN=bantam.local/O=CPNV/OU=Media' -newkey rsa:2048 -keyout $SSLCERTDIR/bantam.local.key -out $SSLCERTDIR/bantam.local.crt
 
 sudo ln -sf /etc/apache2/mods-available/ssl.load /etc/apache2/mods-enabled/ssl.load
 
@@ -50,8 +48,8 @@ sudo ln -sf /etc/apache2/mods-available/ssl.load /etc/apache2/mods-enabled/ssl.l
 sudo ln -sf /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/rewrite.load
 
 # Utilise le php.ini de développement
-sudo mv /etc/php/7.2/apache2/php.ini /etc/php/7.2/apache2.php.ini.bk
-sudo cp /usr/lib/php/7.2/php.ini-development /etc/php/7.2/apache2/php.ini
+sudo mv /etc/php/7.3/apache2/php.ini /etc/php/7.3/apache2.php.ini.bk
+sudo cp /usr/lib/php/7.3/php.ini-development /etc/php/7.3/apache2/php.ini
 
 sudo systemctl restart apache2.service
 
@@ -73,3 +71,6 @@ echo "create database if not exists wp" | mysql -u root -pcpnv4321
 echo "grant all privileges on wp.* to 'cpnvdev'@'localhost' identified by 'cpnv1234' with grant option" | mysql -u root -pcpnv4321
 
 echo "flush privileges" | mysql -u root -pcpnv4321
+
+# Add the user vagrant to the adm group so that it can read logs
+adduser vagrant adm
